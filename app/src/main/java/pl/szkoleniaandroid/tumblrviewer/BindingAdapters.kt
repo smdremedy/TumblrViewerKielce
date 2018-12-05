098f0485eb31d61d700cd8200bc04e1c90adff06
Sylwester Madej <smd@soldiersofmobile.com>
2018-12-05 09:44:51 +0100

    Post Fragment

--- app/src/main/java/pl/szkoleniaandroid/tumblrviewer/BindingAdapters.kt
+++ app/src/main/java/pl/szkoleniaandroid/tumblrviewer/BindingAdapters.kt
@@ -2,14 +2,17 @@ package pl.szkoleniaandroid.tumblrviewer
 
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.Placeholder
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
 
-@BindingAdapter("imageUrl")
-fun loadUrl(view: ImageView, url: String) {
+@BindingAdapter("imageUrl", "placeholder", requireAll = false)
+fun loadUrl(view: ImageView, url: String, placeholder: Int) {
     if (url.isNotEmpty()) {
-        Picasso.get().load(url).into(view)
+        Picasso.get().load(url)
+            .placeholder(if(placeholder == 0) R.mipmap.ic_launcher else placeholder)
+            .into(view)
     }
 }
 
---
+++ app/src/main/java/pl/szkoleniaandroid/tumblrviewer/PostFragment.kt
@@ -0,0 +1,42 @@
+package pl.szkoleniaandroid.tumblrviewer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.fragment.app.Fragment
+
+class PostFragment : Fragment() {
+
+    var postUrl: String = ""
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.post_fragment, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val webView: WebView = view.findViewById(R.id.web_view)
+        webView.loadUrl(postUrl)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        postUrl = arguments?.getString(POST_URL_ARG) ?: ""
+    }
+
+    companion object {
+        private const val POST_URL_ARG = "post_url"
+
+        fun newInstance(postUrl: String) = PostFragment().apply {
+            arguments = Bundle().apply {
+                putString(POST_URL_ARG, postUrl)
+            }
+        }
+    }
+
+
+}
